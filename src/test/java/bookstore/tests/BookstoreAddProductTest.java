531d7cc1fecfabad75fd53e3fb8ce6529e3c389f
LEI-122678 <gcraa@iscte-iul.pt>
2025-11-26 08:25:51 +0000

    Adicionar produto - Test Final

--- src/test/java/bookstore/tests/BookstoreAddProductTest.java
+++ src/test/java/bookstore/tests/BookstoreAddProductTest.java
@@ -3,14 +3,33 @@ package bookstore.tests;
 import bookstore.pages.BookFormPage;
 import bookstore.pages.BookstoreMainPage;
 import bookstore.pages.LoginPage;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 @Epic("Bookstore")
 @Feature("Gestão de Livros")
-public class BookstoreAddProductTest extends BaseTest {
+public class BookstoreAddProductTest {
+
+    @BeforeAll
+    static void setUpAll() {
+
+        Configuration.baseUrl = "https://vaadin-bookstore-example.demo.vaadin.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 8000; // 8 segundos
+
+        // Integração com Allure
+        SelenideLogger.addListener("AllureSelenide",
+                new AllureSelenide()
+                        .screenshots(true)
+                        .savePageSource(true)
+        );
+    }
 
     @Test
     @Story("Adicionar novo livro com sucesso")
